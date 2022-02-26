22a12096392699cb985c985f8d026ca2469a1ceb
zyeadkamal <66483556+zyeadkamal@users.noreply.github.com>
2022-02-26 15:51:43 +0200

    Final Product

---
+++ AbdoEl7atyTests/AbdoEl7atyTests.swift
@@ -0,0 +1,70 @@
+//
+//  AbdoEl7atyTests.swift
+//  AbdoEl7atyTests
+//
+//  Created by mac on 2/26/22.
+//  Copyright © 2022 mac. All rights reserved.
+//
+
+import XCTest
+@testable import AbdoEl7aty
+
+class AbdoEl7atyTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+   func testCategoriesAPI() throws {
+       let exception = expectation(description: "Time out")
+
+       MenuController.shared.fetchCategories { (result) in
+           switch result{
+           case .success(let categories):
+               XCTAssertNotNil(categories)
+               exception.fulfill()
+           case .failure(_):
+               XCTFail()
+           }
+       }
+           waitForExpectations(timeout: 10, handler: nil)
+   }
+   
+   func testMenuItemsAPI() throws {
+       let exception = expectation(description: "Time out")
+
+       MenuController.shared.fetchMenuItems(forCategory: "salads") { (result) in
+           switch result{
+           case .success(let menus):
+               XCTAssertNotNil(menus)
+               exception.fulfill()
+           case .failure(_):
+               XCTFail()
+           }
+       }
+           waitForExpectations(timeout: 10, handler: nil)
+       
+   }
+    
+    
+   func testSubmitAPI() throws {
+       let exception = expectation(description: "Time out")
+       
+       MenuController.shared.submitOrder(forMenuIDs: [1,2]){ (result) in
+           switch result{
+           case .success(let time):
+               XCTAssertNotNil(time)
+               exception.fulfill()
+           case .failure(_):
+               XCTFail()
+           }
+       }
+           waitForExpectations(timeout: 10, handler: nil)
+       
+   }
+
+}
